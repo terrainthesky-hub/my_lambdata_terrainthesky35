8a1a15e782b99f664eefc67cf3227be0fe447946
Lesley Rich <lesley.t.rich@gmail.com>
2020-05-05 13:33:13 -0700

    init file

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="my-package-name", # the name that you will install via pip
-    version="1.0",
-    author="Your Full Name",
+    name="my_lamdata_terrainthesky35", # the name that you will install via pip
+    version="1.35",
+    author="lesley",
     author_email="your@email.com",
     description="A short description",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     #license="MIT",
-    url="https://github.com/YOUR_USERNAME/YOUR_REPO_NAME",
+    url="https://github.com/terrainthesky-hub/my_lambdata_terrainthesky35",
     #keywords="",
     packages=find_packages() # ["my_lambdata"]
 )
