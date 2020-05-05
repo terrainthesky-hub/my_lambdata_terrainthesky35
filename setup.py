3ca7a85168b672e69d9ae258c9b337e884c2331c
Lesley Rich <lesley.t.rich@gmail.com>
2020-05-05 14:12:02 -0700

    fixed directories

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="my_lamdata_terrainthesky35", # the name that you will install via pip
+    name="my_lambdata_terrainthesky35", # the name that you will install via pip
     version="1.36",
     author="lesley",
     author_email="your@email.com",
